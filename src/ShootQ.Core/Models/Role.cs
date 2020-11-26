9bc7fe846951e24c2b17024a128bc29fe319a47b
QuinntyneBrown <quinntynebrown@gmail.com>
2020-11-25 21:48:25 -0500

    add identity controller

--- src/ShootQ.Core/Models/Role.cs
+++ src/ShootQ.Core/Models/Role.cs
@@ -1,17 +1,7 @@
-using BuildingBlocks.Abstractions;
-using System;
-
 namespace ShootQ.Core.Models
 {
-    public class Role: AggregateRoot
+    public class Role
     {
-        protected override void When(dynamic @event) => When(@event);
-
-        protected override void EnsureValidState()
-        {
-
-        }
-
-        public Guid RoleId { get; private set; }
+        public string Name { get; set; }
     }
 }
---
+++ src/ShootQ.Domain/Features/Identity/Authenticate.cs
@@ -0,0 +1,76 @@
+using FluentValidation;
+using MediatR;
+using BuildingBlocks.Abstractions;
+using ShootQ.Core.Models;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
+using BuildingBlocks.Core;
+using System.Security.Claims;
+
+namespace ShootQ.Domain.Features.Identity
+{
+    public class Authenticate
+    {
+        public class Validator : AbstractValidator<Request>
+        {
+            public Validator()
+            {
+                RuleFor(x => x.Username).NotNull();
+                RuleFor(x => x.Password).NotNull();
+            }
+        }
+
+        public class Request : IRequest<Response> {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class Response
+        {
+            public string AccessToken { get; set; }
+            public Guid UserId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Request, Response>
+        {
+            private readonly IAppDbContext _context;
+            private readonly IPasswordHasher _passwordHasher;
+            private readonly ITokenProvider _tokenProvider;
+
+            public Handler(IAppDbContext context, ITokenProvider tokenProvider, IPasswordHasher passwordHasher)
+            {
+                _context = context;
+                _tokenProvider = tokenProvider;
+                _passwordHasher = passwordHasher;
+            }
+
+            public async Task<Response> Handle(Request request, CancellationToken cancellationToken) {
+                
+                var user = _context.Set<User>()
+                    .SingleOrDefault(x => x.Username.ToLower() == request.Username.ToLower());
+
+                if (user == null)
+                    throw new Exception();
+
+                if (!ValidateUser(user, _passwordHasher.HashPassword(user.Salt, request.Password)))
+                    throw new Exception();
+
+                return new Response()
+                {
+                    AccessToken = _tokenProvider.Get(request.Username, user?.Roles.Select(x => new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", x.Name)).ToList()),
+                    UserId = user.UserId
+                };
+            }
+
+            public bool ValidateUser(User user, string transformedPassword)
+            {
+                if (user == null || transformedPassword == null)
+                    return false;
+
+                return user.Password == transformedPassword;
+            }
+        }
+    }
+}
