c20c5281763d92ade6ab8ff246265b814842c7fd
QuinntyneBrown <quinntynebrown@gmail.com>
2020-11-30 08:50:23 -0500

    add equipment

---
+++ src/ShootQ.Core/Models/Equipment.cs
@@ -0,0 +1,21 @@
+using BuildingBlocks.Abstractions;
+using System;
+
+namespace ShootQ.Core.Models
+{
+    public class Equipment: AggregateRoot
+    {
+        protected override void When(dynamic @event) => When(@event);
+
+        protected override void EnsureValidState()
+        {
+
+        }
+
+        public Guid EquipmentId { get; private set; }
+        public string Name { get; set; }
+        decimal Price { get; set; }
+        public string Description { get; set; }
+        public Guid? ReceiptDigitalAssetId { get; set; }
+    }
+}
