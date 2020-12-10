b19364b0e75d93bef3173f5ea300f595d411c59a
QuinntyneBrown <quinntynebrown@gmail.com>
2020-12-09 22:44:21 -0500

    add strip building blocks

---
+++ building-blocks/BuildingBlocks.Stripe/PaymentProcessor.cs
@@ -0,0 +1,62 @@
+﻿using BuildingBlocks.AspNetCore;
+using Stripe;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.Stripe
+{
+    public record PaymentDto(string Number, long? ExpMonth, int ExpYear, string Cvc, int Value, string Description, string Currency);
+    public interface IPaymentProcessor
+    {
+        Task<bool> ProcessAsync(PaymentDto payment);
+    }
+
+    public class PaymentException : HttpStatusCodeException
+    {
+        public PaymentException(string detail)
+            : base((int)HttpStatusCode.BadRequest, "Payment", "Payment Issue", detail)
+        {
+
+        }
+    }
+
+    public class PaymentProcessor : IPaymentProcessor
+    {
+        public async Task<bool> ProcessAsync(PaymentDto payment)
+        {
+            try
+            {
+                var optionsToken = new TokenCreateOptions()
+                {
+                    Card = new CreditCardOptions
+                    {
+                        Number = payment.Number,
+                        ExpYear = payment.ExpYear,
+                        ExpMonth = payment.ExpMonth,
+                        Cvc = payment.Cvc
+                    }
+                };
+
+                var serviceToken = new TokenService();
+                var stripeToken = await serviceToken.CreateAsync(optionsToken);
+                var options = new ChargeCreateOptions
+                {
+                    Amount = payment.Value,
+                    Currency = payment.Currency,
+                    Description = payment.Description,
+                    Source = stripeToken.Id
+                };
+
+                var service = new ChargeService();
+                var charge = await service.CreateAsync(options);
+
+                return charge.Paid;
+            }
+            catch (Exception e)
+            {
+                throw new PaymentException(e.Message);
+            }
+        }
+    }
+}
--- src/ShootQ.Core/Models/Order.cs
+++ src/ShootQ.Core/Models/Order.cs
@@ -1,4 +1,6 @@
 using BuildingBlocks.Abstractions;
+using BuildingBlocks.Stripe;
+using ShootQ.Core.DomainEvents;
 using ShootQ.Core.ValueObjects;
 using System;
 using System.Collections.Generic;
@@ -9,19 +11,43 @@ namespace ShootQ.Core.Models
     {
         protected override void When(dynamic @event) => When(@event);
 
+        public void When(OrderCheckedOut orderCheckedOut)
+        {
+
+        }
+
+        public void When(OrderPaid paid)
+        {
+
+        }
+
         protected override void EnsureValidState()
         {
 
+        }
+
+        public void Checkout(IPaymentProcessor paymentProcessor)
+        {
+            Apply(new OrderCheckedOut());
+
+            Apply(new OrderPaid());
         }
 
         public Guid OrderId { get; private set; }
         public Price Total { get; private set; }
         public Email BillToEmail { get; private set; }
         public ICollection<LineItem> LineItems { get; private set; }
+        public OrderStatus Status { get; private set; }
 
         public static System.Threading.Tasks.Task<Order> FromQuote(Quote quote)
         {
             throw new NotImplementedException();
         }
+    }
+
+    public enum OrderStatus
+    {
+        New,
+        Paid
     }
 }
