c5564c8965f1c82a373d9cf77b4457373b876684
QuinntyneBrown <quinntynebrown@gmail.com>
2021-01-21 23:40:00 -0500

    shorten namespace

--- src/DblDip.Core/Data/EntityConfigurations/SurveyConfiguration.cs
+++ src/DblDip.Core/Data/EntityConfigurations/SurveyConfiguration.cs
@@ -2,7 +2,7 @@ using DblDip.Core.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace DblDip.Core.Data.EntityConfigurations
+namespace DblDip.Core.Data
 {
     public class SurveyConfiguration : IEntityTypeConfiguration<Survey>
     {
