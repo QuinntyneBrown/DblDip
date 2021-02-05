bfda8942152ab74398c5648f39dae43742095d6d
QuinntyneBrown <quinntynebrown@gmail.com>
2021-02-05 02:31:00 -0500

    validation

---
+++ building-blocks/BuildingBlocks.Core/ResponseBase.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BuildingBlocks.Core
+{
+    public class ResponseBase
+    {
+        public List<string> ErrorMessages { get; set; }
+    }
+}
--- building-blocks/BuildingBlocks.Core/ValidationBehavior.cs
+++ building-blocks/BuildingBlocks.Core/ValidationBehavior.cs
@@ -9,13 +9,14 @@ namespace BuildingBlocks.Core
 {
     public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
         where TRequest : IRequest<TResponse>
+        where TResponse :ResponseBase, new()
     {
         private readonly IEnumerable<IValidator<TRequest>> _validators;
 
         public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
             => _validators = validators;
 
-        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
             var context = new ValidationContext<TRequest>(request);
             var failures = _validators
@@ -25,9 +26,19 @@ namespace BuildingBlocks.Core
                 .ToList();
 
             if (failures.Any())
-                throw new ValidationException(failures);
+            {
+                var response = await next();
 
-            return next();
+                foreach(var failure in failures)
+                {
+                    response.ErrorMessages.Add(failure.ErrorMessage);
+                }
+
+                return response;
+            }
+                
+
+            return await next();
         }
     }
 }
