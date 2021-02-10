f0f8c4b516fd7f1d0a11e2939df4435d3307cabd
QuinntyneBrown <quinntynebrown@gmail.com>
2021-02-10 17:32:57 -0500

    add post editor

--- src/DblDip.Frontend/src/app/workspace/blogs/blogs.module.ts
+++ src/DblDip.Frontend/src/app/workspace/blogs/blogs.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PostEditorComponent } from './post-editor/post-editor.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PostEditorComponent],
   imports: [
     CommonModule
   ]
